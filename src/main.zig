616d46c5df5a5eb7d45dc6666191b35c611fa053
pfg <pfg@pfg.pw>
2021-12-21 03:34:45 -0500

    a map. I want to make tools (eg a map editor) in wasm4 but unfortunately you can't do that really

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const w4 = @import("wasm4.zig");
 
-const smiley = [8]u8{
+var smiley = [8]u8{
     0b11000011,
     0b10000001,
     0b00100100,
@@ -11,12 +11,79 @@ const smiley = [8]u8{
     0b11000011,
 };
 
+var stone = [8]u8{
+    0b01111011,
+    0b10000100,
+    0b10000100,
+    0b10010000,
+    0b01111110,
+    0b10010000,
+    0b10010000,
+    0b10000000,
+};
+
+const Tile = enum(u8) {
+    air = ' ',
+    stone = 'X',
+};
+
+const tile_size = w4.Vec2{8, 8};
+const screen_size = @splat(2, @as(i32, w4.CANVAS_SIZE)) / tile_size;
+
+var screen = @ptrCast(*const [screen_size[w4.x] * screen_size[w4.y]]Tile, ""
+    ++ "XXXXXXXXXXXXXXXXXXXX"
+    ++ "X                  X"
+    ++ "X                  X"
+    ++ "X                  X"
+    ++ "X                  X"
+    ++ "X                  X"
+    ++ "X                  X"
+    ++ "X                  X"
+    ++ "X                  X"
+    ++ "X                  X"
+    ++ "XXXXXXX    XXXXXXXXX"
+    ++ "XXXXXXXX    XXXXXXXX"
+    ++ "XXXXXXXXX   XXXXXXXX"
+    ++ "XXXXXXXXX   XXXXXXXX"
+    ++ "XXXXXXXXXXXXXXXXXXXX"
+    ++ "XXXXXXXXXXXXXXXXXXXX"
+    ++ "XXXXXXXXXXXXXXXXXXXX"
+    ++ "XXXXXXXXXXXXXXXXXXXX"
+    ++ "XXXXXXXXXXXXXXXXXXXX"
+    ++ "XXXXXXXXXXXXXXXXXXXX"
+).*;
+
 var state: struct {
     start: ?w4.Vec2 = null,
 } = .{};
 
 export fn update() void {
+    w4.PALETTE.* = .{
+        0x000000,
+        0x555555,
+        0xAAAAAA,
+        0xFFFFFF,
+    };
+
+    w4.DRAW_COLORS.* = 0x20;
+    
+    var x: i32 = 0;
+    while(x < w4.CANVAS_SIZE / tile_size[w4.x]) : (x += 1) {
+        var y: i32 = 0;
+        while(y < w4.CANVAS_SIZE / tile_size[w4.y]) : (y += 1) {
+            const tile = screen[@intCast(usize, y * screen_size[w4.x] + x)];
+            switch(tile) {
+                .air => {},
+                .stone => {
+                    w4.DRAW_COLORS.* = 0x23;
+                    w4.blit(&stone, w4.Vec2{x, y} * tile_size, .{8, 8}, .{.bpp = .b1});
+                },
+            }
+        }
+    }
+
     w4.DRAW_COLORS.* = 0x2;
+
     w4.text("Hello from Zig!", .{10, 10});
 
     const gamepad = w4.GAMEPAD1.*;
@@ -27,7 +94,7 @@ export fn update() void {
         });
     }
 
-    w4.text("Press X to blink", .{16, 90});
+    w4.text("Press X to blink", .{16, 70});
 
     const mouse = w4.MOUSE;
     if(mouse.buttons.left) {
